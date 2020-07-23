b71b1470705b123d8dff4b13d9f99203eb73fc59
IgoVeyner <igorveyner95@gmail.com>
2020-07-23 07:36:38 -0400

    Blank Gem created

---
+++ lib/pokemon_cli_gem.rb
@@ -0,0 +1,6 @@
+require "pokemon_cli_gem/version"
+
+module PokemonCliGem
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/pokemon_cli_gem/version.rb
@@ -0,0 +1,3 @@
+module PokemonCliGem
+  VERSION = "0.1.0"
+end
